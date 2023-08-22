f90725a05d64f499edad211ac0303a841c7db186
taeeonkim <myhom43@gmail.com>
2023-08-22 22:11:24 +0900

    카트 추가, 수정, 삭제

--- app.js
+++ app.js
@@ -6,21 +6,30 @@ const cartsRouter = require('./routes/carts');
 const connect = require("./schemas");
 connect();
 
-app.use(express.json());
+app.use(express.json()); // post, put 전달된 BODY 데이터를 req.body로 사용할 수 있도록 만든 bodyparser
 
-app.use("/api", [goodsRouter, cartsRouter]);
 
-app.get('/', (req, res) => {
-  console.log(req.body)
-  res.status(400).json({test: 'aaaaa'});
-});
+
+app.use("/api", (req, res, next) => {
+  console.log('네번째 미들웨어');
+  next();
+},
+(req, res, next) => {
+  console.log('다번째 미들웨어');
+  next();
+},[goodsRouter, cartsRouter]); // API가 사용되기 위한 라우터를 등록
+
+// app.get('/', (req, res) => {
+//   console.log(req.body)
+//   res.status(400).json({test: 'aaaaa'});
+// });
 
 app.post('/', (req, res) => {
-  console.log(req.body)
+  // console.log(req.body)
   res.json(req.body);
 });
 
-app.get('/:id', (req, res) => {
+app.get('/', (req, res) => {
   console.log(req.params, req.body)
   res.send('Hello World!');
 });
--- routes/goods.js
+++ routes/goods.js
@@ -38,8 +38,51 @@ const router = express.Router();
 //     },
 //   ];
 
-const Goods = require("../schemas/goods");
 
+const Cart = require('../schemas/cart')
+router.post('/goods/:goodsId/cart', async(req, res)=>{
+  const { goodsId } = req.params;
+  const { quantity } = req.body;
+
+  const existsCarts = await Cart.find({goodsId});
+  if(existsCarts.length){
+    return res.status(400).json({
+      success:false,
+      errorMessage: '이미 장바구니에 해당하는 상품이 존재합니다.'})
+  }
+
+  await Cart.create({goodsId, quantity});
+
+  res.json({ result: 'success' });
+
+})
+
+router.put('/goods/:goodsId/cart', async (req, res) => {
+  const { goodsId } = req.params;
+  const { quantity } = req.body;
+
+  const existsCarts = await Cart.find({goodsId});
+  if(existsCarts.length) {
+    await Cart.updateOne(
+      { goodsId: goodsId },
+      { $set: { quantity: quantity }}
+      )
+  }
+  res.status(200).json({ success: true });
+})
+
+router.delete('/goods/:goodsId/cart', async (req, res) => {
+  const { goodsId } = req.params;
+  const existsCarts = await Cart.find({ goodsId });
+
+  if(existsCarts.length) {
+    await Cart.deleteOne({ goodsId: goodsId });
+  }
+
+  res.json({ result: 'success' });
+})
+
+const Goods = require("../schemas/goods");
 
 //POST, GOODs(상품) 추가(등록)
 router.post('/goods', async (req, res) => {
@@ -57,7 +100,12 @@ router.post('/goods', async (req, res) => {
 
 
 //GET, 모든 goods 정보 가져오기
-router.get('/goods', async (req, res) => {
+router.get('/goods', 
+(req, res, next) => {
+  console.log('/goods 미들웨어');
+  next();
+}
+,async (req, res) => {
   const goods = await Goods.find({});
     res.status(200).json(goods)
 })
